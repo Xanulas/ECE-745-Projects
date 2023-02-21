67f67216e8ebba291e3c5a455eac96f3d4e5c06e
Alex Harward <abharwar@ncsu.edu>
2023-02-20 23:07:15 -0500

    done with write state of FSM
    Signed-off-by: Alex Harward <abharwar@ncsu.edu>

--- verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
@@ -26,15 +26,14 @@ endtask
 task wait_for_i2c_transfer ( output i2c_op_t op, output bit [I2C_DATA_WIDTH-1:0] write_data []);
   int listen_counter;
   listen_states_t listen_state;
-  bit [8:0] addr_heard;
-  bit [8:0] data_heard;
+  bit [6:0] addr_heard;
+  bit [7:0] data_heard;
 
   listen_state = IDLE;
 
   while(1)
-  begin
-    @(posedge scl_i)
-      $display("while loop");
+  begin  
+    //$display("===== while begin =====");
     case(listen_state)
     IDLE  : begin
       listen_state = IDLE;
@@ -42,16 +41,31 @@ task wait_for_i2c_transfer ( output i2c_op_t op, output bit [I2C_DATA_WIDTH-1:0]
         if(scl_i) listen_state = ADDR;
     end
     ADDR  : begin
-      for (int i = 0; i < 9; i++) begin
-        @(posedge scl_i)
-          addr_heard[i] = sda_i;
-        if(i == 8) begin
-          drive_sda(1'b0); // ACK (got address)
-          $display("address heard: %b", addr_heard);
-        end
+      wait(scl_i); wait(!scl_i);
+      for (int i = 0; i < 7; i++) begin
+        wait(scl_i); 
+          addr_heard[6-i] = sda_i;
+        wait(!scl_i);
       end
+      $display("addr heard: %h", addr_heard);
+      wait(scl_i); 
+        if(sda_i) op = I2C_READ;
+        else op = I2C_WRITE;
+      wait(!scl_i);  
+      // wait(scl_i); wait(!scl_i);
+        listen_state = WRITE;
+        drive_sda(1'b0); // ack
     end
     WRITE : begin
+      for (int i = 0; i < 8; i++) begin
+        wait(scl_i); 
+          data_heard[7-i] = sda_i;
+        wait(!scl_i);
+      end
+        $display("data heard: %d", data_heard);
+        listen_state = WRITE;
+        // wait(scl_i); wait(!scl_i); 
+        drive_sda(1'b0); // ack
     end
     READ  : begin
     end
